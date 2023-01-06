cd5822c4e7cfeb676c4c90163a2733db50bf8436
Park-hoon <83690999+Park-hoon@users.noreply.github.com>
2023-01-06 21:59:57 +0900

    id 별로 css를 적용시키고 보이지 않게 수정했습니다.

--- test.js
+++ test.js
@@ -2,7 +2,19 @@ var count = localStorage.getItem("count");
 money();
 document.getElementById("count+").addEventListener('click',addcounting);
 document.getElementById("count-").addEventListener('click',subcounting);
+//css 적용
+var id;
+makeid();
+document.getElementById(id).style.display = 'block';
 
+function makeid(){
+    if(count < 7){
+        id = count+'p';
+    }
+    else{
+        id = "7p";
+    }
+}
 
 if(typeof(count) === "object"){
     count = 0;
@@ -25,24 +37,33 @@ function subcounting(){
     }
 }
 
+function nowcount(){
+    document.getElementById('nowcount').innerHTML="";
+    document.getElementById('nowcount').appendChild(document.createTextNode(count));
+    document.getElementById(id).style.display = 'none';
+    makeid();
+    document.getElementById(id).style.display = 'block';
+
+}
+
+//돈 출력
 function money(){
     var money = 1200000;
     var moneydisplay = document.getElementById('display');
     for(var i = 1; i<7; i++){
         money = Math.ceil(money/2/10000)*10000;
-        var count_money = document.createTextNode(i + "회째 : ");
-        var nowmoney = document.createTextNode(money);
-        var br = document.createElement('br');
-        moneydisplay.appendChild(count_money);
-        moneydisplay.appendChild(nowmoney);
-        moneydisplay.appendChild(br);
+        var p = document.createElement('p');
+        p.setAttribute('id', i+'p');
+        p.setAttribute('class', 'neon');
+        var nowmoney = document.createTextNode(i + "회째 : " + money + '원');
+        p.appendChild(nowmoney);
+        moneydisplay.appendChild(p);
     }
-    moneydisplay.appendChild(document.createTextNode("7회 이후는 만원!"));
+    var p = document.createElement('p');
+    var nowmoney = document.createTextNode("7회 이후는 만원!");
+    p.setAttribute('id', '7p');
+    p.setAttribute('class', 'neon');
+    p.appendChild(nowmoney);
+    moneydisplay.appendChild(p);
 }
 document.getElementById('nowcount').appendChild(document.createTextNode(count));
-
-
-function nowcount(){
-    document.getElementById('nowcount').innerHTML="";
-    document.getElementById('nowcount').appendChild(document.createTextNode(count));
-}
